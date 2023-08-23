ec513410f5afe47a0ee7a9925a9d119c72b09f9b
Naresh kumar <naresh.kesa19@gmail.com>
2023-08-23 00:29:19 -0700

    updated

--- 02.print.sh
+++ 02.print.sh
@@ -1,8 +1,8 @@
 echo Hello World
 
-echo -e "\[31mHello World\e[0m"
-echo -e "\[32mHello World\e[0m"
-echo -e "\[33mHello World\e[0m"
-echo -e "\[34mHello World\e[0m"
-echo -e "\[35mHello World\e[0m"
-echo -e "\[36mHello World\e[0m"
+echo -e "\e[31mHello World\e[0m"
+echo -e "\e[32mHello World\e[0m"
+echo -e "\e[33mHello World\e[0m"
+echo -e "\e[34mHello World\e[0m"
+echo -e "\e[35mHello World\e[0m"
+echo -e "\e[36mHello World\e[0m"
